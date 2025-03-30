448509f2d0a4291556bce138a9e421bf12caec9f
loglog050801 <loglog050801@gmail.com>
2025-03-30 22:22:11 +0700

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('kafka:fake-produce')->everyThirtySeconds();
+//        $schedule->command('kafka:fake-produce')->everyThirtySeconds();
         $schedule->command('fake:user')->everyMinute();
 //        $schedule->command('kafka:get-consumer')->everyMinute();
     }
